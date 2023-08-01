9c7151200e6faaff8c9fa99760d8b967d8896374
Danilo Keithi Makiya Nakasone <100208589+DaniloKeithi@users.noreply.github.com>
2023-08-01 12:44:36 -0300

    Add files via upload

---
+++ Funcoes/funcoes.js
@@ -0,0 +1,10 @@
+function dadosacesso(){
+    nuncartão = prompt("Digite o número do cartão: ")
+    senha = prompt("Digite a senha: ")
+}
+
+saldo = 1000
+
+function saque(valor){
+    saldo = saldo - valor
+    return saldo}
